894eb716e3bcb51e47de39aa7f9531d337bd72b7
baby9 <zealand@duck.com>
2023-08-06 18:08:10 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+
+Docker image to run Cloudflare Warp in proxy mode.
+
+## Usage
+
+```
+docker run -d -p 40000:40000 -e LICENSE="" --restart=unless-stopped zenexas/warp-cli:latest
+```
+Input your warp plus LICENSE key
+
+HTTP & SOCKS5 proxy server will be listening at port 40000.
+<br/><br/>
+#### RUN :
+```
+curl -s4m8 -x socks5://127.0.0.1:40000 https://www.cloudflare.com/cdn-cgi/trace | grep warp
+```
+Test your warp account type
+<br/><br/>
+````
+curl -s -x http://127.0.0.1:40000 https://ipinfo.io
+````
+Lookup your warp ip location
+<br/><br/>
+````
+curl -x socks5://127.0.0.1:40000 https://speed.cloudflare.com/__down?bytes=1000000000 > /dev/null
+````
+Speedtest
---
+++ entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+(
+    if [ ! -f /var/lib/cloudflare-warp/reg.json ]; then
+        while ! warp-cli --accept-tos register; do
+            sleep 1
+            >&2 echo "Awaiting warp-svc become online..."
+        done
+    fi
+
+    if [ ! -z $LICENSE ]; then
+        warp-cli --accept-tos set-license $LICENSE
+    fi
+
+    warp-cli --accept-tos set-mode proxy
+    warp-cli --accept-tos set-proxy-port 40001
+    warp-cli --accept-tos connect
+    
+    # socat is used to redirect traffic from 40000 to 40001
+    socat TCP-LISTEN:40000,fork TCP:localhost:40001 &
+    socat UDP-LISTEN:40000,fork UDP:localhost:40001 &
+) &
+
+exec warp-svc
+warp-cli enable-always-on
